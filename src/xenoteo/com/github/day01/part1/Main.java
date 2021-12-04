bc07420e22305341a979459812b8890a77764ba7
xenoteo <fedorovxa@gmail.com>
2021-12-05 00:14:52 +0100

    Day 1 part 1 added

---
+++ src/xenoteo/com/github/day01/part1/Main.java
@@ -0,0 +1,12 @@
+package xenoteo.com.github.day01.part1;
+
+import xenoteo.com.github.InputReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String filePath = "../input/input.txt";
+        int[] depths = InputReader.readToIntArray(Main.class.getResource(filePath));
+        System.out.println(new Solution().countIncreases(depths));
+    }
+}
---
+++ src/xenoteo/com/github/day01/part1/Solution.java
@@ -0,0 +1,14 @@
+package xenoteo.com.github.day01.part1;
+
+public class Solution {
+
+    public int countIncreases(int[] depths) {
+        int counter = 0;
+        for (int i = 1; i < depths.length; i++) {
+            if (depths[i] > depths[i - 1]) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
