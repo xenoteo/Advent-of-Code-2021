bc5cc052b1e9cc4728e8eacdafa4ad2349d1bfa2
xenoteo <fedorovxa@gmail.com>
2021-12-18 14:26:18 +0100

    Day 7 part 2 optimization

--- src/xenoteo/com/github/day07/part2/Solution.java
+++ src/xenoteo/com/github/day07/part2/Solution.java
@@ -11,28 +11,23 @@ public class Solution {
     /**
      * Count the minimum amount of fuel needed to align all crabs to one horizontal position.
      *
-     * Tries each position in the range between crabs min and max position to be chosen as crabs final position,
-     * and finds the minimum fuel needed.
+     * Sets the final position to the mean of initial positions and count the fuel needed.
      *
-     * Time complexity is O(N * D), space complexity is O(1),
-     * where N is the number of crabs and D is the difference between crabs min and max positions.
+     * Time complexity is O(N), space complexity is O(1),
+     * where N is the number of crabs.
      *
      * @param crabs  the array of crabs horizontal positions
      * @return the minimum amount of fuel needed to align all crabs to one horizontal position
      */
     public int minimumFuelSpentToMakeCrabsPositionsMatch(int[] crabs) {
-        int minFuel = Integer.MAX_VALUE;
-        int minPossibleCrab = Arrays.stream(Arrays.stream(crabs).toArray()).min().getAsInt();
-        int maxPossibleCrab = Arrays.stream(Arrays.stream(crabs).toArray()).max().getAsInt();
-        for (int chosenCrab = minPossibleCrab; chosenCrab <= maxPossibleCrab; chosenCrab++) {
-            int fuel = 0;
-            for (int crab : crabs) {
-                int diff = Math.abs(crab - chosenCrab);
-                fuel += (diff + 1) * diff/ 2;
-            }
-            minFuel = Math.min(fuel, minFuel);
+        int sum = Arrays.stream(Arrays.stream(crabs).toArray()).sum();
+        int finalPosition = sum / crabs.length;
+        int fuel = 0;
+        for (int crab : crabs) {
+            int diff = Math.abs(crab - finalPosition);
+            fuel += (diff + 1) * diff/ 2;
         }
-        return minFuel;
+        return fuel;
     }
 
 }
